411ebcf8ae967eb915744967ab780f6cffd91874
Purushottam Tayade <pawan.td@gmail.com>
2016-08-19 17:26:49 +0530

    Initial Commit

--- Calculator/ViewController.h
+++ Calculator/ViewController.h
@@ -8,8 +8,22 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    float valueOne;
+    float valueTwo;
+    float output;
+}
 
+@property (strong, nonatomic) IBOutlet UITextField *textFieldOne;
+@property (strong, nonatomic) IBOutlet UITextField *textFieldTwo;
+@property (strong, nonatomic) IBOutlet UILabel *labelOperator;
+@property (strong, nonatomic) IBOutlet UILabel *labelOutput;
+- (IBAction)buttonMinus:(id)sender;
+- (IBAction)buttonplus:(id)sender;
+- (IBAction)buttonMultiply:(id)sender;
+- (IBAction)buttonDivide:(id)sender;
+- (IBAction)buttonEqualTo:(id)sender;
+- (IBAction)buttonReset:(id)sender;
 
 @end
 
--- Calculator/ViewController.m
+++ Calculator/ViewController.m
@@ -12,6 +12,7 @@
 
 @end
 
+
 @implementation ViewController
 
 - (void)viewDidLoad {
@@ -24,4 +25,75 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)buttonMinus:(id)sender {
+    self.labelOperator.text = @"-";
+    [self.labelOperator setBackgroundColor:[UIColor whiteColor]];
+    
+}
+
+- (IBAction)buttonplus:(id)sender {
+    self.labelOperator.text = @"+";
+    [self.labelOperator setBackgroundColor:[UIColor whiteColor]];
+}
+
+- (IBAction)buttonMultiply:(id)sender {
+    self.labelOperator.text = @"*";
+    [self.labelOperator setBackgroundColor:[UIColor whiteColor]];
+
+}
+
+- (IBAction)buttonDivide:(id)sender {
+    self.labelOperator.text = @"/";
+    [self.labelOperator setBackgroundColor:[UIColor whiteColor]];
+
+}
+
+- (IBAction)buttonEqualTo:(id)sender {
+    [self.textFieldOne resignFirstResponder];
+    [self.textFieldTwo resignFirstResponder];
+
+    NSString *valOne = self.textFieldOne.text;
+    NSString *valTwo = self.textFieldTwo.text;
+
+    NSString *op = self.textFieldOne.text;
+
+    if(!([valOne isEqualToString:@""]) && ![valTwo isEqualToString:@""] && ![op isEqualToString:@""])
+    {   op = self.labelOperator.text;
+    valueOne = valOne.floatValue;
+    valueTwo = valTwo.floatValue;
+        [self.view setBackgroundColor:[UIColor blackColor]];
+if([op isEqualToString:@"+"])
+{
+    output = valueOne + valueTwo;
+    self.labelOutput.text = [NSString stringWithFormat:@"%f",output];
+}else if([op isEqualToString:@"-"])
+{
+    output = valueOne - valueTwo;
+    self.labelOutput.text = [NSString stringWithFormat:@"%f",output];
+}else if([op isEqualToString:@"*"])
+{
+    output = valueOne * valueTwo;
+    self.labelOutput.text = [NSString stringWithFormat:@"%f",output];
+}else if([op isEqualToString:@"/"])
+{
+    output = valueOne / valueTwo;
+    self.labelOutput.text = [NSString stringWithFormat:@"%f",output];
+}
+    }
+    else{
+        NSLog(@"Wrong move");
+        
+    }
+}
+- (IBAction)buttonReset:(id)sender {
+    self.textFieldOne.text = @"";
+    self.textFieldTwo.text = @"";
+    self.labelOutput.text = @"";
+    self.labelOperator.text = @"";
+    
+    [self.labelOperator setBackgroundColor:[UIColor blackColor
+                                        ]];
+
+    [self.view setBackgroundColor:[UIColor whiteColor]];
+}
 @end
